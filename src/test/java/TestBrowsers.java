9481fc032c61c376d7759fd2c9198f55406d6025
Naveenkumar D <naveen@gamil.com>
2025-01-30 21:31:36 +0530

    First commit

---
+++ src/test/java/TestBrowsers.java
@@ -0,0 +1,41 @@
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestBrowsers {
+
+	public static String browser = "Chrome";
+	public static WebDriver driver;
+
+	public static void main(String[] args) {
+
+		if (browser.equals("Chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		}
+
+		if (browser.equals("Firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		}
+
+		if (browser.equals("Edge")) {
+			WebDriverManager.iedriver().setup();
+			driver = new EdgeDriver();
+		}
+		driver.get("http://way2automation.com");
+		String Title = driver.getTitle();
+
+		System.out.println(Title);
+		System.out.println(Title.length());
+
+		//driver.close();
+		driver.quit();
+	}
+
+}
