d22658321b0745e6aac094199fec6dd0fcf3098d
Naveenkumar D <naveen@gamil.com>
2025-01-30 21:49:45 +0530

    Thrid commit

--- src/test/java/TestBrowsers.java
+++ src/test/java/TestBrowsers.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -12,7 +13,8 @@ public class TestBrowsers {
 	public static String browser = "Firefox";
 	public static WebDriver driver;
 
-	public static void main(String[] args) {
+	@Test
+	public void testBrowser() {
 
 		if (browser.equals("Chrome")) {
 			WebDriverManager.chromedriver().setup();
